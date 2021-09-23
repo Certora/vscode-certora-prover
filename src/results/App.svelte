8afde8fd12b0712889342d6794e5c673085233cb
Danil Shashkov <shashkovdanil@gmail.com>
2021-09-23 17:06:01 +0500

    #4: Realize code item component

--- src/results/App.svelte
+++ src/results/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Pane from './components/Pane.svelte'
+  import CodeItemList from './components/CodeItemList.svelte'
 </script>
 
 <Pane
@@ -8,23 +9,61 @@
     { title: 'Action1', icon: '\\ea60', onClick: () => console.log(123) },
     { title: 'Action2', icon: '\\ea97', onClick: () => console.log(456) },
   ]}
->
-  <ul>
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-  </ul>
-</Pane>
+/>
 <Pane title="Smart contract B" />
 <Pane title="Call traces" />
-<Pane title="Variables" />
+<Pane title="Variables">
+  <CodeItemList
+    codeItems={[
+      {
+        name: 'certoraCond19969',
+        value: 'ce4604a0000000000000000000000016',
+        selected: false,
+      },
+      {
+        name: 'CollateralTokenInfoTokenAddress(6)',
+        value: 'ce4604a0000000000000000000000016',
+        selected: true,
+      },
+      {
+        name: 'certoraCond19969',
+        value: 'true',
+        selected: false,
+      },
+      {
+        name: 'certoraCond19969',
+        value: '0',
+        selected: false,
+      },
+      {
+        name: 'certoraCond19969',
+        value: '1',
+        selected: false,
+      },
+    ]}
+  />
+</Pane>
 <Pane title="Contract call resolution warnings" />
 <Pane title="Contract call resolution" />
 
 <style lang="postcss">
   :global(body) {
     padding: 0;
+  }
+
+  :global(body.vscode-light) {
+    --code-item-name-color: #00f;
+    --code-item-value-color: #454545;
+    --code-item-value-background-color: #f0f0f0;
+    --code-item-background-color-selected: #0060c0;
+    --code-item-background-color-hover: #f0f0f0;
+  }
+
+  :global(body.vscode-dark) {
+    --code-item-name-color: #7cb9eb;
+    --code-item-value-color: #ccc;
+    --code-item-value-background-color: #37373d;
+    --code-item-background-color-selected: #094771;
+    --code-item-background-color-hover: #37373d;
   }
 </style>
---
+++ src/results/components/CodeItem.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  export let name: string
+  export let value: string
+  export let selected: boolean
+</script>
+
+<div class="code-item" class:selected tabindex="0" role="button" on:click>
+  <div class="name">{name}</div>
+  <div class="value">{value}</div>
+</div>
+
+<style lang="postcss">
+  .code-item {
+    display: flex;
+    flex-flow: wrap;
+    align-items: center;
+    gap: 2px;
+    cursor: pointer;
+    font-family: Consolas, 'Courier New', monospace;
+    font-size: 12px;
+    line-height: 18px;
+    padding: 6px 0 6px 16px;
+
+    &:hover:not(.selected) {
+      background-color: var(--code-item-background-color-hover);
+    }
+
+    &.selected {
+      background-color: var(--code-item-background-color-selected);
+
+      .name,
+      .value {
+        color: #fff;
+      }
+
+      .value {
+        background-color: transparent;
+      }
+    }
+  }
+
+  .name {
+    color: var(--code-item-name-color);
+  }
+
+  .value {
+    color: var(--code-item-value-color);
+    background-color: var(--code-item-value-background-color);
+    padding: 2px;
+    border-radius: 2px;
+  }
+</style>
