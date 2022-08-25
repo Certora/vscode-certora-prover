b0b6b8d40b2fd5500b0a6862fdfa73d63cca65bc
biced <biced.nff@gmail.com>
2022-08-25 16:03:21 +0300

    solidity contracts

--- src/settings/not_sure_how_to_structure/SolidityFiles.svelte
+++ src/settings/not_sure_how_to_structure/SolidityFiles.svelte
@@ -21,8 +21,8 @@
     loadFilesFolder: loadFilesFolder,
     fileType: 'sol',
     index: index,
-    ifoText: 'some string',
-    infoLink: 'www.google.com',
+    ifoText: infoObjArr.mainFile.infoText,
+    infoLink: infoObjArr.mainFile.infoText,
   }
 
   // push new linking/directory
@@ -51,11 +51,15 @@
   }
 </script>
 
-<div class="card_parent_wrapper bg_dark border-rd">
-  <CollapseCard chevron="padding-right:16px;">
-    <div slot="header" class="header header_contracts">
+<div class="bg_light border-rd mt-8px">
+  <CollapseCard chevron="padding-right:12px;">
+    <div
+      slot="header"
+      class="header header_contract"
+      style="padding: 10px 4px 10px 12px"
+    >
       <i class="codicon codicon-file" />
-      <h3>
+      <h3 style="line-height: 18px; margin-right:auto;">
         {$solAdditionalContracts[index].mainContract ||
           `additional Contract ${index + 1}`}
       </h3>
@@ -64,146 +68,130 @@
         on:click|stopPropagation={() => removeSolFile(index)}
       />
     </div>
-    <div slot="body" class="p-16 pt-0">
-      <div class="bg_light border-rd">
-        <CollapseCard chevron="padding-right:12px;">
-          <div slot="header" class="p-12 header header_contract">
-            <i class="codicon codicon-file" />
-            <h3>Main contract</h3>
+    <div slot="body" class="p-12 pt-0">
+      <div class="input_wrapper">
+        <div class="dark_input">
+          <h3>Source<span>*</span></h3>
+          <button
+            on:click={() => updateItems('sol')}
+            style="background: transparent; padding:0; border:none;"
+          >
+            <Select
+              listOpen={isSolidityListOpen}
+              iconProps={solidityIconsObj}
+              items={solFiles}
+              Item={CustomItem}
+              {Icon}
+              {ClearIcon}
+              on:select={e => handleSelectSol(e, index)}
+              on:clear={e => handleClear(e, index)}
+              placeholder="Additional solidity file"
+              bind:value={$solAdditionalContracts[index].mainFile}
+            />
+          </button>
+        </div>
+        <div class="dark_input">
+          <h3>Main contract name<span>*</span></h3>
+          <CustomInput
+            infoObj={infoObjArr.contractName}
+            placeholder="className()"
+            bind:bindValue={$solAdditionalContracts[index].mainContract}
+          />
+        </div>
+      </div>
+      <div class="border-rd bg_dark mt-8px">
+        <CollapseCard chevron="padding-right:8px;">
+          <div slot="header" class="p-8 header header_contract">
+            <i class="codicon codicon-gear" />
+            <h3>Compiler</h3>
+            <h3 style="margin-left:auto; text-transform:initial;">
+              {$solAdditionalContracts[index].compiler.ver}
+            </h3>
           </div>
-          <div slot="body" class="p-12 pt-0">
+          <div slot="body" class="most_inner_card">
             <div class="input_wrapper">
               <div class="dark_input">
-                <h3>Source<span>*</span></h3>
-                <button
-                  on:click={() => updateItems('sol')}
-                  style="background: transparent; padding:0; border:none;"
-                >
-                  <Select
-                    listOpen={isSolidityListOpen}
-                    iconProps={solidityIconsObj}
-                    items={solFiles}
-                    Item={CustomItem}
-                    {Icon}
-                    {ClearIcon}
-                    on:select={handleSelectSol}
-                    on:clear={e => handleClear(e)}
-                    placeholder="Main solidity file"
-                    bind:value={$solAdditionalContracts[index].mainFile}
-                  />
-                </button>
+                <h3>
+                  Directory containing solidity packages<span>*</span>
+                </h3>
+                <CustomInput
+                  infoObj={infoObjArr.solPackages}
+                  placeholder="CVT-Executables-Mac"
+                  bind:bindValue={$solAdditionalContracts[index].compiler.exe}
+                />
               </div>
               <div class="dark_input">
-                <h3>Main contract name<span>*</span></h3>
+                <h3>Default soldity version to use<span>*</span></h3>
+                <!-- no placeholder, this filed should have the default compiler selected by default -->
                 <CustomInput
-                  infoObj={infoObjArr.contractName}
-                  placeholder="className()"
-                  bind:bindValue={$solAdditionalContracts[index].mainContract}
+                  infoObj={infoObjArr.solCompiler}
+                  placeholder="version: solc7.6"
+                  bind:bindValue={$solAdditionalContracts[index].compiler.ver}
                 />
               </div>
             </div>
-            <div class="border-rd bg_dark mt-8px">
-              <CollapseCard chevron="padding-right:8px;">
-                <div slot="header" class="p-8 header header_contract">
-                  <i class="codicon codicon-gear" />
-                  <h3>Compiler</h3>
-                  <h3 style="margin-left:auto; text-transform:initial;">
-                    {$solAdditionalContracts[index].compiler.ver}
-                  </h3>
+          </div>
+        </CollapseCard>
+      </div>
+      <!-- linking -->
+      <div class="border-rd bg_dark mt-8px">
+        <CollapseCard open={false} chevron="padding-right:8px;">
+          <div slot="header" class="p-8 header header_contract">
+            <i class="codicon codicon-gear" />
+            <h3>Linking</h3>
+          </div>
+          <div slot="body" class="most_inner_card">
+            {#each $solAdditionalContracts[index].linking as obj, index}
+              <div class="input_wrapper mt-8px">
+                <div class="dark_input">
+                  <CustomInput
+                    infoObj={infoObjArr.link}
+                    placeholder="Variable"
+                    bind:bindValue={obj.variable}
+                  />
                 </div>
-                <div slot="body" class="most_inner_card">
-                  <div class="input_wrapper">
-                    <div class="dark_input">
-                      <h3>
-                        Directory containing solidity packages<span>*</span>
-                      </h3>
-                      <CustomInput
-                        infoObj={infoObjArr.solPackages}
-                        placeholder="CVT-Executables-Mac"
-                        bind:bindValue={$solAdditionalContracts[index].compiler
-                          .exe}
-                      />
-                    </div>
-                    <div class="dark_input">
-                      <h3>Default soldity version to use<span>*</span></h3>
-                      <!-- no placeholder, this filed should have the default compiler selected by default -->
-                      <CustomInput
-                        infoObj={infoObjArr.solCompiler}
-                        placeholder="version: solc7.6"
-                        bind:bindValue={$solAdditionalContracts[index].compiler
-                          .ver}
-                      />
-                    </div>
-                  </div>
+                <div class="dark_input">
+                  <CustomInput
+                    infoObj={infoObjArr.link}
+                    placeholder="Contract name"
+                    bind:bindValue={obj.contractName}
+                  />
                 </div>
-              </CollapseCard>
-            </div>
-            <!-- linking -->
-            <div class="border-rd bg_dark mt-8px">
-              <CollapseCard open={false} chevron="padding-right:8px;">
-                <div slot="header" class="p-8 header header_contract">
-                  <i class="codicon codicon-gear" />
-                  <h3>Linking</h3>
-                </div>
-                <div slot="body" class="most_inner_card">
-                  {#each $solAdditionalContracts[index].linking as obj, index}
-                    <div class="input_wrapper mt-8px">
-                      <div class="dark_input">
-                        <CustomInput
-                          infoObj={infoObjArr.link}
-                          placeholder="Variable"
-                          bind:bindValue={obj.variable}
-                        />
-                      </div>
-                      <div class="dark_input">
-                        <CustomInput
-                          infoObj={infoObjArr.link}
-                          placeholder="Contract name"
-                          bind:bindValue={obj.contractName}
-                        />
-                      </div>
-                      <i
-                        class="codicon codicon-trash"
-                        on:click={removeObj(
-                          $solAdditionalContracts[index].linking,
-                          index,
-                        )}
-                      />
-                    </div>
-                  {/each}
-                  <button
-                    class="btn_add"
-                    on:click={pushNewObj(
-                      $solAdditionalContracts[index].linking,
-                      {
-                        variable: '',
-                        contractName: '',
-                      },
-                    )}><i class="codicon codicon-add" /> Add Link</button
-                  >
-                </div>
-              </CollapseCard>
-            </div>
-            <div class="border-rd bg_dark mt-8px">
-              <CollapseCard open={false} chevron="padding-right:8px;">
-                <div slot="header" class="p-8 header header_contract">
-                  <i class="codicon codicon-gear" />
-                  <h3>Specific method</h3>
-                </div>
-                <div slot="body" class="most_inner_card">
-                  <div class="input_wrapper input_single">
-                    <div class="dark_input">
-                      <h3>Function name</h3>
-                      <CustomInput
-                        infoObj={infoObjArr.method}
-                        placeholder="method_name()"
-                        bind:bindValue={$solAdditionalContracts[index]
-                          .specifiMethod}
-                      />
-                    </div>
-                  </div>
-                </div>
-              </CollapseCard>
+                <i
+                  class="codicon codicon-trash"
+                  on:click={removeObj(
+                    $solAdditionalContracts[index].linking,
+                    index,
+                  )}
+                />
+              </div>
+            {/each}
+            <button
+              class="btn_add"
+              on:click={pushNewObj($solAdditionalContracts[index].linking, {
+                variable: '',
+                contractName: '',
+              })}><i class="codicon codicon-add" /> Add Link</button
+            >
+          </div>
+        </CollapseCard>
+      </div>
+      <div class="border-rd bg_dark mt-8px">
+        <CollapseCard open={false} chevron="padding-right:8px;">
+          <div slot="header" class="p-8 header header_contract">
+            <i class="codicon codicon-gear" />
+            <h3>Specific method</h3>
+          </div>
+          <div slot="body" class="most_inner_card">
+            <div class="input_wrapper input_single">
+              <div class="dark_input">
+                <h3>Function name</h3>
+                <CustomInput
+                  infoObj={infoObjArr.method}
+                  placeholder="method_name()"
+                  bind:bindValue={$solAdditionalContracts[index].specifiMethod}
+                />
+              </div>
             </div>
           </div>
         </CollapseCard>
