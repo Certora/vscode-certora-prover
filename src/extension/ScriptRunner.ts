1f83c8956218868471e9c78749909829fa33f27d
NoaBablikiCertora <you@example.com>
2023-01-29 14:43:13 +0200

    added parse/type errors to PROBLEMS

--- src/extension/ScriptRunner.ts
+++ src/extension/ScriptRunner.ts
@@ -8,9 +8,9 @@ import { ResultsWebviewProvider } from './ResultsWebviewProvider'
 import { getProgressUrl } from './utils/getProgressUrl'
 import { Job, CERTORA_INNER_DIR, LOG_DIRECTORY_DEFAULT } from './types'
 import { PostProblems } from './PostProblems'
-import { checkDir } from './utils/checkDir'
 import fetch from 'node-fetch'
 import * as os from 'os'
+import { getInternalDirPath } from './utils/getRunInternalInfo'
 
 type RunningScript = {
   pid: number
@@ -43,67 +43,25 @@ export class ScriptRunner {
   }
 
   /**
-   * returns a Date if possible, null if not possible
-   * @param dir string in the format of 22_12_05_14_22_09_BLA
-   * @returns date object
-   */
-  private getDateFormat(dir: string): [Date, string] {
-    const arr = dir.split('_').slice(0, 6)
-    const dateStr = '20' + arr.slice(0, 3).join('-') + 'T'
-    const timeStr = arr.slice(3, 6).join(':') + 'Z'
-    const str = dateStr + timeStr
-    return [new Date(str), dir]
-  }
-
-  /**
    * returns a uri of the conf.log file if the workspace path exists, null otherwise
    * @param pathToConfFile path to the .conf file (relative)
    * @param ts the time the file was created
    * @returns the full path to the conf.log file or null
    */
   private async getLogFilePath(pathToConfFile: string, ts: number) {
-    const path = workspace.workspaceFolders?.[0]
-    if (!path) return
-
-    const internalUri = Uri.parse(path.uri.path + CERTORA_INNER_DIR)
-    const checked = await checkDir(internalUri)
-    if (checked) {
-      const innerDirs = await workspace.fs.readDirectory(internalUri)
-      const dates = innerDirs.map(dir => {
-        if (dir[1] === 2) {
-          return this.getDateFormat(dir[0])
-        }
-        return null
-      })
-      // filter out null values
-      const datesNew = dates.filter(date => {
-        return date && date[0]
-      })
-
-      // sort by date
-      const sortedDates = datesNew.sort(function (a, b) {
-        if (a !== null && b !== null) {
-          return a[0] > b[0] ? -1 : 1
-        }
-        return 0
-      })
-
-      // get the most recent date / dir
-      const curDate = sortedDates[0]
-
-      if (curDate) {
-        const logFilePath = Uri.joinPath(
-          path.uri,
-          CERTORA_INNER_DIR,
-          curDate[1],
-          `${this.getConfFileName(pathToConfFile)}.log`,
-        )
-        return logFilePath
-      }
+    let path = await getInternalDirPath()
+    if (path) {
+      const logFilePath = Uri.joinPath(
+        path,
+        `${this.getConfFileName(pathToConfFile)}.log`,
+      )
+      return logFilePath
     }
     // if we can't find the relevant directory in [CERTORA_INNER_DIR] --> create log file in another directory named [LOG_DIRECTORY_DEFAULT] inside it. in this case we need timestamps to be not run over older log files
+    path = workspace.workspaceFolders?.[0]?.uri
+    if (!path) return
     const logFilePath = Uri.joinPath(
-      path.uri,
+      path,
       CERTORA_INNER_DIR,
       LOG_DIRECTORY_DEFAULT,
       `${this.getConfFileName(pathToConfFile)}-${ts}.log`,
